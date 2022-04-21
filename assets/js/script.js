d266c44927500dc33b961c9264f7cf581b137af2
tdawes93 <tdawes136@googlemail.com>
2022-04-21 06:56:17 +0000

    Add modals and error modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,66 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener(("click"), e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+function processOptions(form) {
+    let optArray = [];
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+    form.append("options", optArray.join());
+    return form;
+}
+
+function displayException(data) {
+    let heading = `An Exception Occured`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
+
+async function postForm(e) {
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+    let results = "";
+    let heading = `JSHint results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no-errors">No errors reported</div>`;
+    } else {
+        results = `<div>Total Errors:<span class="error-count">${data.total_errors}</span></div>`
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -11,7 +71,7 @@ async function getStatus(e) {
 
     const data = await response.json();
 
-    if(response.ok) {
+    if (response.ok) {
         displayStatus(data);
     } else {
         throw new Error(data.error);
@@ -24,3 +84,4 @@ function displayStatus(data) {
     <div class="key-status"> ${data.expiry}</div>`
     resultsModal.show();
 }
+
